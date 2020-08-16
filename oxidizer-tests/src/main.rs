791e7383f0b5b3e286b1ad011e4b48dfc5aac0c9
Gustavo Sampaio <gbritosampaio@gmail.com>
2020-08-16 13:32:49 -0300

    Add missing files

---
+++ oxidizer-tests/src/main.rs
@@ -0,0 +1,26 @@
+
+use tokio;
+use oxidizer::*;
+
+#[derive(Entity, Default)]
+pub struct ABC {
+    #[primary_key]
+    pub id: i32,
+    pub name: String,
+    pub addr1: String,
+    pub addr2: String,
+}
+
+
+
+fn main() {}
+
+mod test {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_abc() {
+        let mut abc = ABC::default();
+    }
+
+}
