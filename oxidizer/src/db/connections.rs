967b54cf7830b5b02410c19ca9bc0af145e9521e
super-horst <super-horst@posteo.net>
2020-10-03 00:59:40 +0200

    cleanup & happy clippy

--- oxidizer/src/db/connections.rs
+++ oxidizer/src/db/connections.rs
@@ -13,7 +13,16 @@ pub fn create_connection_provider(
     ca_file: Option<&str>,
 ) -> Result<Box<dyn ConnectionProvider>, Error> {
     let prov: Box<dyn ConnectionProvider> = if let Some(ca_file) = ca_file {
-        new_tls(config, ca_file)?
+        cfg_if::cfg_if! {
+            if #[cfg(feature = "tls-openssl")] {
+                tls_openssl::new_tls(config, ca_file)?
+            } else if #[cfg(feature = "tls-rustls")] {
+                tls_rustls::new_tls(config, ca_file)?
+            }else {
+                eprintln!("[WARN] no TLS provider found, reverting to unencrypted connection");
+                no_tls(config)
+            }
+        }
     } else {
         no_tls(config)
     };
@@ -38,31 +47,18 @@ fn no_tls(config: Config) -> Box<dyn ConnectionProvider> {
     Box::new(NoTlsConnectionProvider { config })
 }
 
-cfg_if::cfg_if! {
-    if #[cfg(not(any(feature = "tls-openssl", feature = "tls-rustls")))] {
-        fn new_tls(config: Config, ca_file: &str) -> Result<Box<dyn ConnectionProvider>, Error> {
-            eprintln!("[WARN] no TLS provider configured");
-            Ok(no_tls(config))
-        }
-    }
-}
-
-cfg_if::cfg_if! {
-if #[cfg(feature = "tls-openssl")] {
-
-use tls_openssl::new_tls;
-
-pub(crate) mod tls_openssl {
+#[cfg(feature = "tls-openssl")]
+mod tls_openssl {
     use super::*;
 
     use openssl::ssl::{SslConnector, SslMethod, SslOptions};
 
-    use tokio_postgres::{Client, Config};
     use postgres_openssl::MakeTlsConnector;
+    use tokio_postgres::{Client, Config};
 
     struct OpensslConnectionProvider {
-        pub(crate) config: Config,
-        pub(crate) tls: MakeTlsConnector,
+        config: Config,
+        tls: MakeTlsConnector,
     }
 
     #[async_trait]
@@ -75,10 +71,8 @@ pub(crate) mod tls_openssl {
     }
 
     pub fn new_tls(config: Config, ca_file: &str) -> Result<Box<dyn ConnectionProvider>, Error> {
-        let mut builder = SslConnector::builder(SslMethod::tls())
-            .map_err(Error::OpensslError)?;
+        let mut builder = SslConnector::builder(SslMethod::tls()).map_err(Error::OpensslError)?;
 
-        builder.set_options(SslOptions::NO_TLSV1_3);
         builder.set_ca_file(ca_file).map_err(Error::OpensslError)?;
 
         Ok(Box::new(OpensslConnectionProvider {
@@ -87,13 +81,8 @@ pub(crate) mod tls_openssl {
         }))
     }
 }
-}}
 
-cfg_if::cfg_if! {
-if #[cfg(feature = "tls-rustls")] {
-
-use tls_rustls::new_tls;
-
+#[cfg(feature = "tls-rustls")]
 mod tls_rustls {
     use super::*;
 
@@ -128,16 +117,10 @@ mod tls_rustls {
             .add_pem_file(&mut reader)
             .map_err(|_| Error::RustlsError("Failed to read certificate file".to_string()))?;
 
-        for root in &root_store.roots {
-            println!("{:?}", root);
-        }
-
         tls_conf.root_store = root_store;
-
         Ok(Box::new(RustlsConnectionProvider {
             config,
             tls: MakeRustlsConnect::new(tls_conf),
         }))
     }
 }
-}}
