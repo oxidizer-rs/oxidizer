4e535121db93857e566c272360decdc649a9eb52
super-horst <super-horst@posteo.net>
2020-10-03 01:18:19 +0200

    TLS integration test

--- oxidizer-tests/src/main.rs
+++ oxidizer-tests/src/main.rs
@@ -20,6 +20,7 @@ pub struct TestCustomPrimaryKey2 {
 
 fn main() {}
 
+#[cfg(test)]
 mod test {
     use super::*;
 
@@ -27,4 +28,30 @@ mod test {
     // async fn test_abc() {
     //     let mut abc = ABC::default();
     // }|
+
+    ///
+    /// Integration test for postgres TLS support (manual invocation only)
+    ///
+    /// Requires:
+    ///  - run of ./generate-pki.sh in ./.devcontainer/dev-pki
+    ///  - start of ./.devcontainer/docker-compose-tls.yml db
+    ///
+    //#[tokio::test]
+    async fn test_tls() {
+        let db = DB::connect(
+            "postgres://postgres:alkje2lkaj2e@localhost:5432/postgres",
+            50,
+            Some("../.devcontainer/dev-pki/ca.cert"),
+        )
+        .await
+        .unwrap();
+
+        db.migrate_tables(&[TestCustomPrimaryKey::create_migration().unwrap()])
+            .await
+            .unwrap();
+
+        let mut entity = TestCustomPrimaryKey::default();
+        let creating = entity.save(&db).await.unwrap();
+        assert_eq!(creating, true);
+    }
 }
